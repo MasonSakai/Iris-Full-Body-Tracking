d2d792c7ed0648f8e00873b428fe1dffcbca6a7f
Mason Sakai <masonsakai@gmail.com>
2025-03-06 21:42:40 -0500

    Initial models working

--- Iris-FBT-Core/Iris_FBT_Core.py
+++ Iris-FBT-Core/Iris_FBT_Core.py
@@ -1,1 +1,44 @@
+from config import Config
 
+from app import create_app, db
+from app.model.yolo import YOLOModel
+
+import sqlalchemy as sqla
+import sqlalchemy.orm as sqlo
+import cv2 as cv
+
+app = create_app(Config)
+
+@app.shell_context_processor
+def make_shell_context():
+    return {'sqla': sqla, 'sqlo': sqlo, 'db': db}
+
+@app.before_request
+def initDB(*args, **kwargs):
+    if app._got_first_request:
+        db.create_all()
+
+def test_model():
+    model = YOLOModel(Config)
+    with model as m:
+        cap = cv.VideoCapture(0)
+        
+        ret, img = cap.read()
+        if not ret:
+            print("failed to grab frame")
+        else:
+            res = m.forward(img)
+            for person in res:
+                for key in person:
+                    data = person[key]
+                    print(f'{key}: {data}')
+                    cv.circle(img, (int(data.x), int(data.y)), 5, (0, 0,255), int(2.5*data.conf))
+                    cv.putText(img, key, (int(data.x), int(data.y)), cv.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0,255), int(data.conf), cv.LINE_AA)
+            
+            cv.imshow("res", img)
+            cv.waitKey()
+        
+if __name__ == "__main__":
+    test_model()
+
+    # app.run(debug=True)
---
+++ Iris-FBT-Core/app/model/yolo.py
@@ -0,0 +1,60 @@
+import os
+import cv2 as cv
+from config import Config
+from ultralytics import YOLO
+from app.model import Model, ModelConfig
+
+
+class YoloModelConfig(ModelConfig):
+    MODEL_PATH = 'Models\\yolo11n-pose.onnx'
+    MODEL_CUTOFF = 0.8
+    MODEL_INDEXES = {
+            'nose':            0,
+            'left_eye':        1,
+            'right_eye':       2,
+            'left_ear':        3,
+            'right_ear':       4,
+            'left_shoulder':   5,
+            'right_shoulder':  6,
+            'left_elbow':      7,
+            'right_elbow':     8,
+            'left_wrist':      9,
+            'right_wrist':    10,
+            'left_hip':       11,
+            'right_hip':      12,
+            'left_knee':      13,
+            'right_knee':     14,
+            'left_foot':      15,
+            'right_foot':     16,
+        }
+
+class YOLOModel(Model):
+    
+    def __init__(self, base_config=Config, model_config=YoloModelConfig):
+        super().__init__(base_config, model_config)
+        path = os.path.join(base_config.ROOT_PATH, model_config.MODEL_PATH)
+        self.model = YOLO(path)
+        print(path)
+
+    
+    def forward(self, img, point_correction=lambda x: x, stream=False, **values):
+        res = self.model.track(img, stream=stream)
+        
+        poses = []
+
+        for r in res:
+            points = r.keypoints
+            for data in points.data:
+                poses.append(self.To_Pose(data, point_correction))
+
+        return poses
+
+    
+    def To_Pose(self, data, point_correction):
+        pose = {}
+        for key in self.model_config.MODEL_INDEXES:
+            pdata = data[self.model_config.MODEL_INDEXES[key]]
+            if pdata[2] >= self.model_config.MODEL_CUTOFF:
+                pose[key] = Model.To_Point(pdata, point_correction)
+                
+        return pose
